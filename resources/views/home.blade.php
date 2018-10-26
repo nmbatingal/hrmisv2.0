f84913f37676152a1c22778e6ed2e06dc23dacf6
nmbatingal <batingalnarz11@gmail.com>
2018-10-26 16:07:39 +0800

    updated home dashboard

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,14 +1,47 @@
 @extends('layouts.app')
 
+@section('styles')
+@endsection
+
 @section('content')
-<div class="card">
-    <div class="card-header">
-        Dashboard
+<!-- ============================================================== -->
+<!-- Bread crumb and right sidebar toggle -->
+<!-- ============================================================== -->
+<!-- <div class="row page-titles">
+    <div class="col-md-12">
+        <h4 class="text-white">List of Applicants</h4>
     </div>
-    <div class="card-body">
-        <h4 class="card-title">Special title treatment</h4>
-        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-        <a href="#" class="btn btn-primary">Go somewhere</a>
+    <div class="col-md-6">
+        <ol class="breadcrumb">
+            <li class="breadcrumb-item"><a href="{{ url('/home') }}">Home</a></li>
+            <li class="breadcrumb-item"><a href="{{ route('applicants.dashboard') }}">HR-Applicants</a></li>
+            <li class="breadcrumb-item active">Add Applicant</li>
+        </ol>
+    </div>
+</div> -->
+<!-- ============================================================== -->
+<!-- End Bread crumb and right sidebar toggle -->
+<!-- ============================================================== -->
+<!-- ============================================================== -->
+<!-- Over Visitor, Our income , slaes different and  sales prediction -->
+<!-- ============================================================== -->
+<div class="m-t-40"></div>
+<div class="row">
+    <!-- Column -->
+    <div class="col-lg-12">
+        <div class="card">
+		    <div class="card-header">
+		        Dashboard
+		    </div>
+		    <div class="card-body">
+		        <h4 class="card-title">Special title treatment</h4>
+		        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+		        <a href="#" class="btn btn-primary">Go somewhere</a>
+		    </div>
+		</div>
     </div>
 </div>
+@endsection
+
+@section('scripts')
 @endsection
