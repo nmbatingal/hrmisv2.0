353a83fa6dc99f3a7c940b70dc82dab8739de321
nmbatingal <batingalnarz11@gmail.com>
2018-10-26 17:14:56 +0800

    update

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -30,15 +30,20 @@
     <!-- Column -->
     <div class="col-lg-12">
         <div class="card">
-		    <div class="card-header">
-		        Dashboard
-		    </div>
-		    <div class="card-body">
-		        <h4 class="card-title">Special title treatment</h4>
-		        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-		        <a href="#" class="btn btn-primary">Go somewhere</a>
-		    </div>
-		</div>
+            <div class="card-header">
+                Dashboard
+            </div>
+            <div class="card-body">
+                <h4 class="card-title">Special title treatment</h4>
+                <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                <a href="#" class="btn btn-primary">Go somewhere</a>
+
+                <div class="visible-print text-center">
+                    {!! QrCode::size(100)->generate(Request::url()) !!}
+                    <p>Scan me to return to the original page.</p>
+                </div>
+            </div>
+        </div>
     </div>
 </div>
 @endsection
