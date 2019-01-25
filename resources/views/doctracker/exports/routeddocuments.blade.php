45a688384420a32bf29cb279e2a6d6eb0f254e05
nmbatingal <batingalnarz11@gmail.com>
2019-01-25 13:52:35 +0800

    update

--- resources/views/doctracker/exports/routeddocuments.blade.php
+++ resources/views/doctracker/exports/routeddocuments.blade.php
@@ -2,27 +2,36 @@
     <thead>
     <tr>
         <th>Tracking Code</th>
-        <th>Subject</th>
+        <th>Action</th>
+        <th>Datetime</th>
         <th>Document Type</th>
         <th>Created by</th>
-        <th>Action Performed</th>
+        <th>Subject</th>
+        <th>Forwarded To</th>
         <th>Action By</th>
         <th>Remarks</th>
         <th>Notes</th>
-        <th>Datetime</th>
     </tr>
     </thead>
     <tbody>
     @foreach($documentsLog as $log)
         <tr>
             <td>{{ $log->tracking_code }}</td>
-            <td>{{ $log->documentCode->subject }}</td>
+            <td>{{ $log->action }}</td>
+            <td>{{ $log->created_at }}</td>
             <td>{{ $log->documentCode->documentType->document_name }}</td>
             <td>{{ $log->documentCode->userEmployee->fullName }}</td>
-            <td>{{ $log->action }}</td>
+            <td>{{ $log->documentCode->subject }}</td>
+            <td>
+                @if ( !is_null( $log->recipients ) )
+                    @foreach( $log->recipients as $recipient)
+                        <li>{{ $recipient['name'] }}</li>
+                    @endforeach
+                @endif
+            </td>
             <td>{{ $log->userEmployee->fullName }}</td>
             <td>{{ $log->remarks }}</td>
-            <td>{{ $log->created_at }}</td>
+            <td>{{ $log->notes }}</td>
         </tr>
     @endforeach
     </tbody>
